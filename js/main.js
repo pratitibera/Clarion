a7f734a790ebca2cedf82d495d390cc3b5f7113a
Pratiti Bera <remabimal0801@gmail.com>
2022-04-07 20:43:08 +0530

    -

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
-$('#header_carousel').owlCarousel({
-    loop: true,
+var $owl = $('.owl-carousel');
+
+$owl.children().each(function (index) {
+    $(this).attr('data-position', index); // NB: .attr() instead of .data()
+});
+
+$owl.owlCarousel({
     center: true,
+    loop: true,
+    items: 3,
     autoplay: true,
     autoPlaySpeed: 1000,
     autoplayHoverPause: true,
@@ -15,4 +22,8 @@ $('#header_carousel').owlCarousel({
             items: 3
         }
     }
+});
+
+$(document).on('click', '.owl-item>div', function () {
+    $owl.trigger('to.owl.carousel', $(this).data('position'));
 });
