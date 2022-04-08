4feda34fe6a453a7458a412bae2de9024a99914a
Pratiti Bera <remabimal0801@gmail.com>
2022-04-08 11:43:28 +0530

    -

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-document.getElementById('user_date').value = "bbb";
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById('user_date').value = today;
+
 var $owl = $('.owl-carousel');
 
 $owl.children().each(function (index) {
